dea37953df76a84f1e410c20738fba9d162d480b
ssekridov@gmail.com <ssekridov@gmail.com>
2023-01-02 14:09:36 +0100

    feat: 04-date-tasks 3/5 solve

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,9 +19,7 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
-}
+const parseDataFromRfc2822 = (value) => new Date(value);
 
 /**
  * Parses an ISO 8601 string date representation into date value
@@ -34,10 +32,7 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
-}
-
+const parseDataFromIso8601 = (value) => Date.parse(value);
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +48,11 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
-}
+const isLeapYear = (date) => {
+  const year = new Date(date).getFullYear();
 
+  return new Date(year, 1, 29).getDate() === 29;
+};
 
 /**
  * Returns the string representation of the timespan between two dates.
